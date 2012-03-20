a7e47af95239b008212ee4ad970e85955d83f049
Atul Varma <varmaa@gmail.com>
2012-03-20 15:47:34 -0500

    Origination.

---
+++ app.js
@@ -0,0 +1,27 @@
+var request = require('request'),
+    path = require('path'),
+    express = require('express'),
+    config = require('./config');
+
+var app = express.createServer();
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/article', function (req, res) {
+  var url = req.query.url;
+  request.get({
+    uri: "http://www.readability.com/api/content/v1/parser" +
+         "?token=" + encodeURIComponent(config.readabilityApiToken) +
+         "&url=" + encodeURIComponent(url),
+    form: true
+  }, function (err, response, body) {
+    try {
+      body = JSON.parse(body);
+    } catch (e) {
+      return res.send('Bad Gateway', 502);
+    }
+    return res.send(body, response.statusCode);
+  });
+});
+
+module.exports = app;
